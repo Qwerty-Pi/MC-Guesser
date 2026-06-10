9ada1c5863bfafaaaa5c4a0e9bca63d7f0c03c7a
Qwerty-Pi <qwertypi@LAPTOP-SLB41II5.>
2026-06-11 04:55:48 +0800

    fix types

---
+++ update_topic.php
@@ -0,0 +1,16 @@
+<?php
+    $data = json_decode(trim(file_get_contents("php://input")), true);
+
+    if (!isset($data['year']) || !$data['year'] || !isset($data['q']) || !$data['q'])
+        die(0);
+
+    $year = $data['year'];
+    $q = $data['q'];
+    $topic = $data['topic'];
+
+    $file = "artifact/paper-2/$year/topics.json";
+    $content = json_decode(file_get_contents($file));
+    $content[$q] = $topic;
+    file_put_contents($file, json_encode($content));
+
+    var_dump("Success");
