ef827292b78ca1995f1cb0e3100fb2167f9a8c4f
Qwerty-Pi <qwertypi@LAPTOP-SLB41II5.>
2026-06-11 03:16:12 +0800

    topic

--- ask_gemini.py
+++ ask_gemini.py
@@ -1,16 +1,31 @@
 from google import genai
 from google.genai import types
 import os, httpx, sys
+import json
 
-def ask(question):
-    client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"], http_options={"timeout": 300_000}) # 5 minutes
+client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"]) # 5 minutes
+
+def ask(prompt):
     response = client.models.generate_content(
-        model='gemini-flash-latest',
-        contents=question
+        model='gemini-3.5-flash',
+        contents=prompt
     )
+    print(response)
     return response.text
 
-paper = "2012-PP"
-prompt = f"Can you give me the answer to DSE {paper} Mathematics Paper 2 in array?"
-resp = ask(prompt)
-print(resp)
+def by_topic(year):
+    prompt = open("topics_J.json", "r").read() + open("topics_S.json", "r").read()
+    prompt += "\nRefers to the above documents, classify the topics listed below, and return as an array of the format J1, S3 where J refers to junior, S refers to senior.\n"
+    prompt += open(f"artifact/paper-2/{year}/questions.html").read()
+    res = ask(prompt)
+    return json.loads(res[7:-3])
+
+def write_by_topic(year):
+    path = f"artifact/paper-2/{year}/topics.json"
+    content = by_topic(year)
+    with open(path, "w") as f:
+        f.write(json.dumps(content))
+
+if len(sys.argv) == 2:
+    year = sys.argv[1]
+    write_by_topic(year)
