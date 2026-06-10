2964f8e039a8560ab6d394a26271cccded9be0c3
Qwerty-Pi <qwertypi@LAPTOP-SLB41II5.>
2026-06-10 08:51:46 +0800

    add answers

--- ask_gemini.py
+++ ask_gemini.py
@@ -8,17 +8,9 @@ def ask(question):
         model='gemini-flash-latest',
         contents=question
     )
-    print("Wait for responses...")
-    res = response.text
-    for candidate in response.candidates:
-        finish_reason = candidate.finish_reason
-        print(f"Finish Reason: {finish_reason}")
-        
-        # Check if generation was cut off due to token limits
-        if finish_reason == "MAX_TOKENS":
-            print("Warning: Response was truncated because it reached max_output_tokens limit.")
-    return res
+    return response.text
 
-prompt = open("prompt.txt", "r").read() + open("artifact/paper-2/2012-PP/merged.txt", "r").read()
+paper = "2012-PP"
+prompt = f"Can you give me the answer to DSE {paper} Mathematics Paper 2 in array?"
 resp = ask(prompt)
 print(resp)
